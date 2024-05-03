5f1ccc44d072f5a11cb9e8be9a0e3f4e2c73491c
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-05-03 14:44:06 +0800

    feat: fix-attempt-2

--- preprocess.sh
+++ preprocess.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# echo "Applying environment variables"
+echo "Applying environment variables"
 
-# echo "DATA_SERVER: $DATA_SERVER"
-# echo "GAME_SERVER: $GAME_SERVER"
-# echo "DATA_SERVER_ROOT: $DATA_SERVER_ROOT"
+echo "DATA_SERVER: $DATA_SERVER"
+echo "GAME_SERVER: $GAME_SERVER"
+echo "DATA_SERVER_ROOT: $DATA_SERVER_ROOT"
 
-# jq --arg DATA_SERVER "$DATA_SERVER" '.Backend = $DATA_SERVER' /www/StreamingAssets/DeploymentConfig.json > tmp.$$.json && mv tmp.$$.json /www/StreamingAssets/DeploymentConfig.json
-# jq --arg GAME_SERVER "$GAME_SERVER" '.Server = $GAME_SERVER' /www/StreamingAssets/DeploymentConfig.json > tmp.$$.json && mv tmp.$$.json /www/StreamingAssets/DeploymentConfig.json
+jq --arg DATA_SERVER "$DATA_SERVER" '.Backend = $DATA_SERVER' /www/StreamingAssets/DeploymentConfig.json > tmp.$$.json && mv tmp.$$.json /www/StreamingAssets/DeploymentConfig.json
+jq --arg GAME_SERVER "$GAME_SERVER" '.Server = $GAME_SERVER' /www/StreamingAssets/DeploymentConfig.json > tmp.$$.json && mv tmp.$$.json /www/StreamingAssets/DeploymentConfig.json
 
-# sed -i "s/dmcc-be.int2.lv-aws-x3.xyzapps.xyz/$DATA_SERVER_ROOT/g" /www/index.html
-
-# echo "Running nginx"
-
-# nginx -g 'daemon off;'
+sed -i "s/dmcc-be.int2.lv-aws-x3.xyzapps.xyz/$DATA_SERVER_ROOT/g" /www/index.html
 
 echo "Running nginx"
+
+nginx -g 'daemon off;'
